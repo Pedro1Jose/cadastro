f33df66820ac7798e58222467ae2aaf05dd4a70a
Pedro1Jose <pedro123jose321@gmail.com>
2026-04-28 10:24:21 -0300

    Controller criado

---
+++ src/main/java/dev/queijo/Cadastro/Controller.java
@@ -0,0 +1,15 @@
+package dev.queijo.Cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Esta é a página de boas-vindas!";
+    }
+}
